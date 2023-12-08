2eaef202892d1a7c80a9656bcd43eeb712e9fcc5
Esen Meredow <esca656585@gmail.com>
2023-12-08 17:29:38 +0500

    updated

--- resources/views/front-end/main.blade.php
+++ resources/views/front-end/main.blade.php
@@ -4,14 +4,6 @@
     {{ __('Dashboard') }}
 @endsection
 
-@section('slider')
-    @include('layouts.slider')
-@endsection
-
-@section('provide-section')
-    @include('layouts.provide-section')
-@endsection
-
 @section('product-area-2')
     @include('layouts.product-area-2')
 @endsection
