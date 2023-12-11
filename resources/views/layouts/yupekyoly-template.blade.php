8fa76dbc5e1a5d15276ed426445481bb61a2f257
Esen Meredow <esca656585@gmail.com>
2023-12-11 10:09:08 +0500

    updated

--- resources/views/layouts/yupekyoly-template.blade.php
+++ resources/views/layouts/yupekyoly-template.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>yupekyoly.com | @yield('title')</title>
-    <meta name="description" content="">
+    <meta name="description" content="yupekyoly.com">
+    <meta name="google-site-verification" content="3Omz1--xfp2OmP5DgKMTPXCr-F69HaNo_5w8JeuaKhU" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Place favicon.ico in the root directory -->
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('baychay-template/img/logo/yupekyoly-logo.png') }}">
