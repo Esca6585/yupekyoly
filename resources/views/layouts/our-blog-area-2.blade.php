925a1323198f7d6d97e9ef8741f193ec4f160f62
Esen Meredow <esca656585@gmail.com>
2023-12-18 17:31:24 +0500

    updated

--- resources/views/layouts/our-blog-area-2.blade.php
+++ resources/views/layouts/our-blog-area-2.blade.php
@@ -4,19 +4,22 @@
         <div class="row">
             <div class="brand-active owl-carousel">
                 <div class="col">
-                    <a href="#"><img src="{{ asset('baychay-template/img/logo/pinduoduologo.png') }}" alt="{{ asset('baychay-template/img/logo/pinduoduologo.png') }}"></a>
+                    <a href="https://ashgabattimes.com" target="_blank"><img src="{{ asset('baychay-template/img/logo/pinduoduologo.png') }}" alt="{{ asset('baychay-template/img/logo/pinduoduologo.png') }}"></a>
                 </div>
                 <div class="col">
-                    <a href="#"><img src="{{ asset('baychay-template/img/logo/poizonlogo.jpg') }}" alt="{{ asset('baychay-template/img/logo/poizonlogo.jpg') }}"></a>
+                    <a href="https://ashgabattimes.com" target="_blank"><img src="{{ asset('baychay-template/img/logo/poizonlogo.jpg') }}" alt="{{ asset('baychay-template/img/logo/poizonlogo.jpg') }}"></a>
                 </div>
                 <div class="col">
-                    <a href="#"><img src="{{ asset('baychay-template/img/logo/taobaologo.webp') }}" alt="{{ asset('baychay-template/img/logo/taobaologo.webp') }}"></a>
+                    <a href="https://ashgabattimes.com" target="_blank"><img src="{{ asset('baychay-template/img/logo/ashgabattimes-logo.png') }}" alt="{{ asset('baychay-template/img/logo/ashgabattimes-logo.png') }}"></a>
                 </div>
                 <div class="col">
-                    <a href="#"><img src="{{ asset('baychay-template/img/logo/alibabalogo.png') }}" alt="{{ asset('baychay-template/img/logo/alibabalogo.png') }}"></a>
+                    <a href="https://ashgabattimes.com" target="_blank"><img src="{{ asset('baychay-template/img/logo/taobaologo.webp') }}" alt="{{ asset('baychay-template/img/logo/taobaologo.webp') }}"></a>
                 </div>
                 <div class="col">
-                    <a href="#"><img src="{{ asset('baychay-template/img/logo/aliexpresslogo.png') }}" alt="{{ asset('baychay-template/img/logo/aliexpresslogo.png') }}"></a>
+                    <a href="https://ashgabattimes.com" target="_blank"><img src="{{ asset('baychay-template/img/logo/alibabalogo.png') }}" alt="{{ asset('baychay-template/img/logo/alibabalogo.png') }}"></a>
+                </div>
+                <div class="col">
+                    <a href="https://ashgabattimes.com" target="_blank"><img src="{{ asset('baychay-template/img/logo/aliexpresslogo.png') }}" alt="{{ asset('baychay-template/img/logo/aliexpresslogo.png') }}"></a>
                 </div>
             </div>
         </div>
